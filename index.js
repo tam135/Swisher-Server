c0094dfa6d8bdbee59c3bb7ed9f0d3bcfcdd1254
Tam Phan <tnphan0204@gmail.com>
2022-09-14 17:57:17 -0400

    set up db and server

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
-const { ApolloServer } = require('apollo-server')
-const gql = require('graphql-tag')
+const { ApolloServer } = require('apollo-server');
+const gql = require('graphql-tag');
 const mongoose = require('mongoose');
 
+const Post = require('./models/Post');
+const { MONGODB } = require('./config.js');
+
 const typeDefs = gql`
+    type Post {
+        id: ID!
+        body: String!
+        createdAt: String!
+        username: String!
+
+    }
     type Query {
-        sayHi: String!
+        getPosts: [Post]
     }
 `
 
 const resolvers = {
     Query: { 
-        sayHi: () => 'Hello World !!!!!'
+        async getPosts(){
+            try {
+                const posts = await Post.find();
+                return posts;
+            } catch(err){
+                throw new Error(err);
+            }
+        }
     }
 }
 
@@ -19,9 +36,14 @@ const server = new ApolloServer({
     resolvers
 });
 
-mongoose.connect()
-
-server.listen({ port:5000 })
+mongoose
+    .connect(MONGODB, { useNewUrlParser: true })
+    .then(() =>{
+        console.log('MongoDB Connected');
+        return server.listen({ port: 5000 });
+    })
     .then(res => {
         console.log(`Server running at ${res.url}`)
     })
+
+   
